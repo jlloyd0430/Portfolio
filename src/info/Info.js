73155974f45e49407651919410c9bcbc33d27806
setsuko <116366380+jlloyd0430@users.noreply.github.com>
2024-09-12 13:19:01 -0400

    Update Info.js

--- src/info/Info.js
+++ src/info/Info.js
@@ -7,21 +7,9 @@ import meme from "../img/memegen.jpeg";
 import mungo from "../img/mungovan.jpeg";
 import dogepond from "../img/dogepond.png";
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
 
 export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
 
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
   firstName: "Jesse",
@@ -62,8 +50,6 @@ export const info = {
       icon: "fa fa-twitter",
       label: "twitter",
     },
-    // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-    // Just change the links so that they lead to your social profiles.
   ],
   bio: "Hello! I'm Jesse.Im a driven self-starter with professional adaptability, full-stack software developer, Eager to grow and learn more about the web3 and tech industry.",
   skills: {
@@ -104,11 +90,8 @@ export const info = {
       label: "music",
       emoji: "🎸",
     },
-    // Same as above, change the emojis to match / relate to your hobbies or interests.
-    // You can also remove the emojis if you'd like, I just think they look cute :P
   ],
   portfolio: [
-    // This is where your portfolio projects will be detailed
     {
       title: "Doginal app",
       live: "https://dogepond.com",
